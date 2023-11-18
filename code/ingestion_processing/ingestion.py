8cb9d8ce0e81f4ddb761d9fcca54ec60b1e8057d
fridrichmrtn <fridrich.mrtn@gmail.com>
2023-11-18 09:04:13 +0100

    Finalize ingestion script.

--- code/ingestion_processing/ingestion.py
+++ code/ingestion_processing/ingestion.py
@@ -1,166 +1,176 @@
-# set for ocr
-def get_tools():
-    import glob
-    import os
-    import shutil
-    import requests
+### prepare
+import pandas as pd
+import re
+import os
+import requests
+from concurrent.futures import ThreadPoolExecutor
+import magic
+from joblib import Parallel, delayed
+import pickle
 
-    file_set = set(glob.glob("*"))
-    if "gs" not in file_set: # gs
-        gs_url = "https://github.com/ArtifexSoftware/ghostpdl-downloads/releases/download/gs9540/ghostscript-9.54.0-linux-x86_64.tgz"
-        r = requests.get(gs_url, allow_redirects=True)
-        open("gs.tgz", "wb").write(r.content)
-        os.system("tar -xzvf gs.tgz")
-        os.remove("gs.tgz")
-        shutil.move("ghostscript-9.54.0-linux-x86_64/gs-9540-linux-x86_64","gs")
-        shutil.rmtree("ghostscript-9.54.0-linux-x86_64", ignore_errors=True)
-    if "eng.traineddata" not in file_set: # lstm
-        tes_url = "https://github.com/tesseract-ocr/tessdata_best/raw/master/eng.traineddata"
-        r = requests.get(tes_url, allow_redirects=True)
-        open("eng.traineddata", "wb").write(r.content)
-
-# load input data
-def get_meta(path):
-    import pandas as pd
-
-    def sanitize_names(r):
-        import re
-
-        ind = str(r.name).rjust(4,"0")
-        year = str(r.Year)
-        part = r.Participant
-        part = part.lower()
-        part = re.sub("[.!?\\-/,\"\(\)\+'\|]"," ",part)
-        part = re.sub("&"," and ",part)
-        part = re.sub(" +","-",part)
-        part = re.sub("-$","",part)
-        pp = [ind, year, part]
-        res = "-".join(pp)
-        if len(r.FileType)>0:
-            res = res+"."+r.FileType
-        return res
-
-    docs = pd.read_excel("../../data/un-global-impact.xlsx", sheet_name=[0,1])
-    docs = pd.concat([v.loc[:,"Participant":"EconomyType"] for k,v in docs.items()])
-    # get only the eng ones with a link
-    docs = docs[(docs.Language=="english") & (docs.Link.notnull())].\
-        sort_values(["Year", "Participant", "Link"]).drop_duplicates("Link").\
-            reset_index(drop=True)
-    # file types
-    file_type = docs.Link.str.split("/").apply(lambda x: x[-1].split("?")[0].split(".")[-1]).str.lower()
-    docs["FileType"] = ["" if f not in set(["pdf", "docx", "pptx", "doc", "docm", "htm", "html"]) else f\
-        for f in file_type.values]
-    docs["FileName"] = docs.apply(sanitize_names, axis=1)
-    return docs
-
-# download papers       
-def download_docs(docs, path):
-    import os
-    import requests
-    import datetime
-    import concurrent.futures
-
-    def download_doc(ind=0, df=docs, output_path=path):
-        r = requests.get(df.loc[ind].Link, allow_redirects=True)
-        open(output_path+df.loc[ind].FileName, "wb").write(r.content)
-
-    # par-all
-    st = datetime.datetime.now()
-    if not os.path.exists(path):
-        os.mkdir(path)    
-    with concurrent.futures.ThreadPoolExecutor(max_workers=8) as exe: 
-        exe.map(download_doc,  docs.index) # docs.index
-    td = (datetime.datetime.now()-st).seconds/60
+class DataIngestion():
     
-    # NOTE ADD STATUS CHECK
-    print("The download took {:.2f} mins on the threadpool back-end.".format(td))
+    def __init__(self,
+                docs_file = "../../data/unglobalcompact.csv",
+                raw_folder = "../../data/raw_files/",
+                data_folder = "../../data/") -> None:
+        self.docs_file = docs_file
+        self.raw_folder = raw_folder
+        pass
 
-def unify_format(docs, path):
-    import magic
-    import numpy as np
-    import datetime
-    import os
-
-    docs["FileTypeMagic"] = docs.FileName.apply(lambda x: magic.from_file(path+x,
-        mime=True))
-    st = datetime.datetime.now()
-    docs["ConversionStatus"] = np.nan
-    docs["ConvertedFileName"] = docs["FileName"] # prefill
-    docs_to_convert_index = docs.loc[(docs.FileTypeMagic!="inode/x-empty") & (docs.FileTypeMagic!="application/pdf")].index
-    for i in docs_to_convert_index:
-        input_file = path+docs.loc[i].FileName
-        output_file = path+docs.loc[i].FileName.split(".")[0]+".pdf"
-        try:
-            cmd = "unoconv -f pdf -T 30 -o "+output_file+" "+input_file
-            docs.loc[i,"ConversionStatus"] = os.system(cmd)
-            docs.loc[i,"ConvertedFileName"] = output_file       
-        except:
-            print("Conversion of the file {} failed.".format(input_file))
-            docs.loc[i,"ConvertedFileName"] = np.nan   
-            continue
-        os.system("pkill soffice.bin")
-    td = (datetime.datetime.now()-st).seconds/60
-    pdf_count = np.sum(docs.ConversionStatus==0)
-    pdf_remainder_count = len(docs_to_convert_index)-pdf_count
-    print("Type conversion finished in {:.2f} mins with {} pdf files and the remainder of {} raw files.".\
-        format(td, pdf_count, pdf_remainder_count,))
-    return docs
-
-def do_ocr(docs, txt_path, pdf_path):
-    import os
-    import datetime
-    import numpy as np
-    import pandas as pd
-
-    def pdf2txt(row, txt_path, pdf_path):
-        import os
-        import numpy as np
+    def _sanitize_name(self, name):
+        name = re.sub(r"[^a-zA-Z ]","", name).lower()
+        return re.sub(r"[ ]+", "_", name)
+    
+    def _get_file_atts(self, path):
+        dir = os.path.dirname(path)
+        base_name = os.path.basename(path)
+        name, extension = os.path.splitext(base_name)
+        return {"dir" : dir,
+                "name" : name,
+                "ext" : extension.split("?")[0]}
+    
+    def _get_destination(self, row, dir=None):
+        atts_dict = self._get_file_atts(row["communication_on_progress_file"])
+        index = str(row.name)
+        company =self._sanitize_name(row["name"])
+        ext = atts_dict["ext"]
+        if dir is None:
+            dir = ""
+        return dir + "_".join([index, company]) + ext
+    
+    def load_docs(self):
+        self.docs_data = pd.read_csv(self.docs_file)   
+        new_columns = {c: self._sanitize_name(c) for c in self.docs_data.columns}
+        self.docs_data = self.docs_data.rename(columns=new_columns)
+        url_rows = self.docs_data.communication_on_progress_file.notnull()
+        self.docs_data = self.docs_data.loc[url_rows,
+                ["name","type", "country", "sector", "communication_on_progress_file"]]
+        self.docs_data["file_destination"] = self.docs_data\
+            .apply(self._get_destination, dir=self.raw_folder, axis=1)
+        return self
         
-        input_file = pdf_path+row.ConvertedFileName
-        output_file =  txt_path+row.FileName.split(".")[0]+".txt"
-        # try to ocr the files
+    def _download_file(self, url, file_path):
+        if os.path.exists(file_path):
+            print(f"File on '{file_path}' already exists. Skipping download.")
+            return None
         try:
-            cmd = "./gs -sDEVICE=ocr -r200 -dQUIET -dBATCH -dNOPAUSE -sOutputFile="+\
-                output_file+" "+input_file
-            row["OCRStatus"] = os.system(cmd)
-            row["OutputFileName"] = output_file
-            with open(output_file, "r") as of:
-                row["OutputText"] = of.read().replace("\n", " ")
-        except:
-            print("OCR of the file {} failed.".format(input_file))
-            row["OutputFileName"] = np.nan
+            response = requests.get(url, stream=True)
+            response.raise_for_status()
+            with open(file_path, 'wb') as file:
+                for chunk in response.iter_content(chunk_size=8192):
+                    file.write(chunk)
+            print(f"File on '{file_path}' downloaded successfully.")
+            return file_path
+        except requests.RequestException as e:
+            print(f"Error downloading '{file_path}': {e}.")
+            return None
+
+    def download_reports(self):
+        url_list = self.docs_data["communication_on_progress_file"].values
+        path_list = self.docs_data["file_destination"].values
+        with ThreadPoolExecutor(max_workers=5) as executor:
+            executor.map(self._download_file, url_list, path_list)
+        return self
+    
+    def _check_path(self,path):
+        if os.path.exists(path):
+            return path
+        else:
+            return None
+
+    def _get_conversion_path(self,file_destination, dir_name=None):
+        if dir_name==None:
+            dir_name = os.path.dirname(file_destination)
+        base_name = os.path.basename(file_destination)
+        name, extension = os.path.splitext(base_name)
+        return os.path.join(*[dir_name, name+".pdf"])
+
+    def _get_metadata(self):
+        self.docs_data["file_type"] = self.docs_data\
+            .apply(lambda x:magic.from_file(x["file_destination"],mime=True), axis=1)
+        self.docs_data["file_size"] = self.docs_data\
+            .apply(lambda x:os.path.getsize(x["file_destination"])/10**6, axis=1)
+        self.docs_data["converted_file_destination"] = None
+        pdf_rows = self.docs_data.file_type=="application/pdf"
+        self.docs_data.loc[pdf_rows, "converted_file_destination"] = self.docs_data.loc[pdf_rows, "file_destination"]
+        conv_rows_ind = self.docs_data.loc[
+            (self.docs_data.converted_file_destination.isnull())
+            & (~self.docs_data.file_type.isin(["inode/x-empty", "application/octet-stream"]))
+            & (self.docs_data.file_size<90),].index
+        self.docs_data["conversion"] = False
+        self.docs_data.loc[conv_rows_ind, "conversion"] = True
+        return self
+
+    def _convert_row(self, ind):
+        file_destination = self.docs_data.loc[ind, "file_destination"]
+        converted_file_destination = self._get_conversion_path(file_destination)
+        converted_file_dir = os.path.dirname(converted_file_destination)
+        # NOTE: make sure this works
+        if os.path.exists(converted_file_destination):
+            print(f"File on '{converted_file_destination}' already exists. Skipping conversion.")
+            self.docs_data.loc[ind, "converted_file_destination"] = converted_file_destination
+            return self
+        try:
+            cmd = f"libreoffice --headless --convert-to pdf {file_destination} --outdir {converted_file_dir}"
+            status = " ".join(os.popen(cmd).readlines())
+            if "error" in status.lower():
+                raise Exception(status)
+            self.docs_data.loc[ind, "converted_file_destination"] = self._check_path(converted_file_destination)
+            print(f"File on '{converted_file_destination}' converted successfully.")
+        except Exception as e:
+            print(f"Error converting '{file_destination}': {e}.")
+        return self
+
+    def convert_reports(self):
+        self = self._get_metadata()
+        for ind in self.docs_data.loc[self.docs_data.conversion,].index:
+            self = self._convert_row(ind)
+        return self
+
+    def _get_txt_path(self, file_path,
+            dir_name="../../data/txt_files/"):
+        base_name = os.path.basename(file_path)
+        name, extension = os.path.splitext(base_name)
+        return os.path.join(*[dir_name, name+".txt"])
+    
+    def _read_row(self, ind, overwrite=False):
+        row = self.docs_data.loc[ind,]
+        row["txt_file_destination"] = None
+        pdf_path = row["converted_file_destination"]
+        txt_path = self._get_txt_path(pdf_path)
+        if os.path.exists(txt_path) and not overwrite:
+            print(f"File on '{txt_path}' already exists. Skipping reading.")
+            row["txt_file_destination"] = txt_path
             return row
-        return row    
+        else:
+            print(f"Reading '{row['converted_file_destination']}'.")
+            try:
+                #txt = self._pdf2txt(pdf_path)
+                #self._txt2file(txt, txt_path)
+                cmd = f"gs -sDEVICE=ocr -r200 -dQUIET -dBATCH -dNOPAUSE -sOutputFile={txt_path} {pdf_path} > conversion.log 2>&1"
+                status = os.system(cmd)
+                row["txt_file_destination"] = self._check_path(txt_path)
+            except Exception as e:
+                print(f"Error reading '{pdf_path}': {e}.")
+        return row
 
-    st = datetime.datetime.now()
-    if not os.path.exists(pdf_path):
-        os.mkdir(pdf_path)   
-    if not os.path.exists(txt_path):
-        os.mkdir(txt_path)
-    docs["OCRStatus"] = np.nan
-    docs["OutputText"] = np.nan
-    docs["OutputFileName"] = np.nan
-    from joblib import Parallel, delayed
-    temp_docs = Parallel(n_jobs=6, backend="loky")(
-        delayed(pdf2txt)(docs.loc[i,:], txt_path, pdf_path) for i in docs.index)
-    docs = pd.concat(temp_docs, axis=1).transpose()    
-    td = (datetime.datetime.now()-st).seconds/60
-    text_count = docs.shape[0]
-    text_remainder_count = docs.OutputText.isna().sum()
-    print("Text conversion finished in {:.2f} mins with {} pdf files and the remainder of {} raw files.".\
-        format(td, text_count-text_remainder_count, text_remainder_count))
-    return docs    
+    def read_reports(self, overwrite=False):
+        rows_ind = self.docs_data.index[self.docs_data.converted_file_destination.notnull()]
+        rows_ls = Parallel(n_jobs=7)(delayed(self._read_row)\
+            (ind, overwrite) for ind in rows_ind)
+        self.docs_data = self.docs_data.merge(
+            pd.DataFrame(rows_ls)[["txt_file_destination"]],
+            how="left", left_index=True, right_index=True)
+        return self
 
-if __name__=="__main__":
-    raw_path, txt_path = "../../data/raw_reports/", "../../data/txt_reports/"
+    def save_data(self, file_path=None):
+        if file_path is None:
+            file_path = self.data_path+"ingested.pkl"
+        with open(file_path, "wb") as file:
+            pickle.dump(self.docs_data, file)
+        return self        
 
-    import shutil
-    shutil.rmtree(raw_path, ignore_errors=True)
-    shutil.rmtree(txt_path, ignore_errors=True)
-    get_tools()
-    docs = get_meta("../../data/un_global_impact.xlsx")
-    #docs = docs.iloc[:250,:]
-    download_docs(docs, raw_path)
-    docs = unify_format(docs, raw_path)
-    #docs = do_ocr(docs, txt_path, raw_path)
-    #docs.to_pickle("../../data/texts.pkl")
+# %%
+# Ingestion = DataIngestion().load_docs().download_reports()\
+#     .convert_reports().read_reports().save_data()
